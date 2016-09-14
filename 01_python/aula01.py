7bd6d43cdd17b513da95fe4a03979d1d443ceb58
Moisés Souto <moises.souto@gmail.com>
2016-09-14 16:05:51 -0300

    Adicionando a primeira aula

---
+++ 01_python/aula01.py
@@ -0,0 +1,6 @@
+# O primeiro scirpt python
+import sys #importa um modulo (biblioteca adicional)
+print(sys.platform) #imprime a versao da plataforma S.O.
+print(2**100)	#Potencia de 2 elevado a 100
+x='Spam!'		#define uma string com conteudo spam
+print(x*4)		#imprimindo a variavel x, por quatro vezes
