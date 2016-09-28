6b8c3041657ac65bbbe1505b39ddf091e9f24afe
Moisés Souto <moises.souto@gmail.com>
2016-09-28 15:59:58 -0300

    Adicionando tecnicas de slice e recorte em strings python

---
+++ 01_python/aula02b.py
@@ -0,0 +1,8 @@
+s='abcdefghijklmnop'
+#Slice e recorte, com passo de 2
+# por padrão, quando não é definido o passo,
+#este acontece de 1 em 1
+print(s[1:10:2])
+print(s[::2])
+print(s[::-1])
+print(s[::-2])
