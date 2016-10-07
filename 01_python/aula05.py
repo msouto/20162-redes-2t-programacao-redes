f7be231474b66516e04ac4485e7a6771f49dcf0c
Moisés Souto <moises.souto@gmail.com>
2016-10-07 17:19:52 -0300

    Adicionando utilização de dicionários em python

---
+++ 01_python/aula05.py
@@ -0,0 +1,9 @@
+#Dicionário
+D = {'nome' : 'joao', 'idade' : 25}
+E = {'nome' : 'joao', 'idade' : '25'}
+print(dir(D['idade']))
+print('\n')
+print(dir(E['idade']))
+
+print(D['idade'])
+print(E['idade'])
