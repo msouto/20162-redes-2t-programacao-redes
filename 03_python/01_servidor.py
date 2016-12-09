8ebace47193f17a436bc904916c364ffb9505735
Moisés Souto <moises.souto@gmail.com>
2016-12-09 16:31:43 -0300

    Exemplo de servidor TCP/IP usando Python

--- 03_python/01_servidor.py
+++ 03_python/01_servidor.py
@@ -1,13 +1,15 @@
 import socket
-HOST, PORT = 127.0.0.1,2000
+HOST = socket.gethostbyname('localhost')
+PORT = 2000
 
 tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_server_socket.bind(HOST,PORT)
-tcp_server_socket.listen(1)
+tcp_server_socket.bind((HOST,PORT))
+tcp_server_socket.listen()
 conn, addr = tcp_server_socket.accept()
-print('Conexão de:'+addr)
-while 1:
+print('Conexão de:', addr)
+while True:
     data = conn.recv(1024)
     if not data: break
     print("\n Mensagem recebido:", data)
-conn.close
+conn.close()
+tcp_server_socket.close()
