8319c3019df962b5450e49ce912e9d440caee794
Moisés Souto <moises.souto@gmail.com>
2016-12-16 17:46:58 -0300

    Ajustes no servidor UDP

--- 03_python/02_servidor_udp.py
+++ 03_python/02_servidor_udp.py
@@ -1,5 +1,5 @@
 import socket
-serverHost = socket.gethostbyname('localhost')
+serverHost = socket.gethostbyname('10.25.1.233')
 serverPort = 12000
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,6 +11,8 @@ print('O servidor está pronto para receber:')
 
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
+    print('Conexão de:', clientAddress)
     #processa a string para letras maiúsculas
     modifiedMessage = message.upper()
+    print("\n", modifiedMessage)
     serverSocket.sendto(modifiedMessage, clientAddress)
